6604bac10e2959f64c104ef26cbf9b3c285da919
AgusAhi <aluahi5617@ieselcaminas.org>
2024-03-14 20:50:22 +0100

    primer commit

---
+++ src/snake/Snake.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package snake;
+
+
+/**
+ *
+ * @author alu10825617
+ */
+public class Snake {
+    
+    
+}
