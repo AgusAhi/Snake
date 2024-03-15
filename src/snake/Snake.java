38720c72956ae6298994429982bb56bee5c91811
AgusAhi <aluahi5617@ieselcaminas.org>
2024-03-15 19:11:13 +0100

    segundo commit

--- src/snake/Snake.java
+++ src/snake/Snake.java
@@ -4,12 +4,46 @@
  */
 package snake;
 
+import java.awt.*;
+
 
 /**
  *
  * @author alu10825617
  */
 public class Snake {
+    
+    private List<Node> nodes;
+    private Direction direction;
+    private int nodesToGrow;
+    
+    public Snake() {
+       nodes = new ArrayList<Node>();
+       nodes.add(new Node(Board.NUM_ROWS/2, Board.NUM_COLS/2));
+       nodes.add(new Node(Board.NUM_ROWS/2, Board.NUM_COLS/2));
+       nodes.add(new Node(Board.NUM_ROWS/2, Board.NUM_COLS/2));
+       nodes.add(new Node(Board.NUM_ROWS/2, Board.NUM_COLS/2));
+       direction = Direction.RIGHT;
+       nodesToGrow = 0;
+    }
+    
+    public void paint(Graphics g, int squareWidth, int squareHeight) {
+        boolean firstNode = true;
+        Color color;
+        for (Node node: nodes) {
+            if (firstNode) {
+                color = Color.orange;
+                firstNode = false;
+            } else {
+                color = Color.yellow;
+            }
+            Util.drawSquare(g, nodesToGrow, nodesToGrow, color, squareWidth, squareHeight);
+        }
+    }
+ 
+    
+    
+           
     
     
 }
